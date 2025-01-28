c4523c8a474de59e8c13b6e0cfc18b0788742b7a
Nicholla <nicollascl@hotmail.com>
2025-01-27 21:15:29 -0300

    Primeiro commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+
+      <h1> Qualquer coisa </h1>
+      <p> Qualquer coisa mesmo </p>
+     
+    </div>
+  );
+}
